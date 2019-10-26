3ae038a0dafca4c68227e2837d24e9cb1537800b
NicolasAsis <nico.asis15@gmail.com>
2019-10-26 12:29:20 -0700

    Started basic folder structure

---
+++ pages/Page-login.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {View,Text,Button,Image} from 'react-native';
+
+function Login() {
+    return(
+        <View>
+            <Text>Hi</Text>
+        </View>
+    );
+}
+
+export default Login;
