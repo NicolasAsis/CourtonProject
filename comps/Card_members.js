3790dc3e141a958c794ecf166b581ac8c698f2d8
siyayang0420 <siyayang0420@live.com>
2019-11-01 20:47:08 -0700

    fix page_group_info
    created bar comps and card_members(card_organizer)

---
+++ comps/Card_members.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+function Card_members(props) {
+  const styles = StyleSheet.create({
+    //Card container
+    cardContainer: {
+      width: 302,
+      height: 68,
+      borderRadius: 10,
+      // backgroundColor:'blue',
+      display: "flex",
+      flexDirection: "row"
+    },
+    //Card container shadow
+    cardContainerShadow: {
+      width: 302,
+      height: 68,
+      borderRadius: 10,
+      position: "relative",
+      backgroundColor: "#FFFFFF",
+      display: "flex",
+      flexDirection: "row",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowOpacity: 0.07,
+      shadowRadius: 10,
+      elevation: 5
+    },
+    //Organizer image section
+    organizerImgSec: {
+      flex: 1,
+      borderRadius: 10,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+      // backgroundColor:'yellow'
+    },
+    //Organizer image
+    organizerImg: {
+      width: 52,
+      height: 52,
+      borderRadius: 50,
+      backgroundColor: "skyblue"
+    },
+    //Organizer name section
+    organizerNameSec: {
+      flex: 3,
+      borderRadius: 10,
+      // backgroundColor:'green',
+      display: "flex",
+      justifyContent: "center"
+    },
+    //"Organizer" text
+    organizerText: {
+      fontFamily: "Open sans",
+      fontSize: 12,
+      lineHeight: 16,
+      color: "#FE647B"
+    },
+    //Name of organizer text
+    organizerNameText: {
+      fontFamily: "Open sans",
+      fontSize: 18,
+      lineHeight: 25,
+      color: "#333333"
+    }
+  });
+  return (
+    //Overall container
+    <View style={{ display: "flex", alignItems: "center", marginBottom: 20 }}>
+      {/* Card Container */}
+      <View style={styles.cardContainer}>
+        <View style={styles.cardContainerShadow}>
+          {/* Organizer Image Section */}
+          <View style={styles.organizerImgSec}>
+            <View style={styles.organizerImg}></View>
+          </View>
+          {/* Organizer Name Section */}
+          <View style={styles.organizerNameSec}>
+            <Text style={styles.organizerText}>{props.organizer}</Text>
+            <Text style={styles.organizerNameText}>Toby Wong</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default Card_members;
--- pages/Page-groupInfo.js
+++ pages/Page-groupInfo.js
@@ -8,8 +8,9 @@ import {
   ScrollView
 } from "react-native";
 
-import Button_Join from '../comps/Button_Join';
-import Card_organizer from '../comps/Card_organizer';
+import Button_Join from "../comps/Button_Join";
+import Card_members from "../comps/Card_members";
+import Bar_group_countdown_price from "../comps/Bar_group_countdown_price";
 
 function GroupInfo() {
   const styles = StyleSheet.create({
@@ -65,61 +66,28 @@ function GroupInfo() {
       top: 190
     },
 
-    //Page subheader
-
-    giSubHeader: {
-      width: "100%",
-      height: 55,
-      backgroundColor: "#FE647B",
-      textAlign: "center",
-      display: "flex",
-      alignItems: "center",
-      flexDirection: "row"
-    },
-    giclosingtimeText: {
-      fontFamily: "Open sans",
-      fontSize: 15,
-      color: "#FFFFFF",
-
-      left: 85
-    },
-    gispectimeText: {
-      color: "#81EC8D",
-      fontWeight: "bold"
-    },
-    groupcloseIcon: {
-      width: 30,
-      height: 30,
-      left: 26
-    },
-    gicostText: {
-      fontFamily: "Open sans",
-      fontSize: 30,
-      fontWeight: "bold",
-      color: "#FFFFFF",
-
-      left: 144
-    },
     //Group Information Section
     giTextSec: {
-      width: 337,
+      width: "100%",
       height: 250,
       // backgroundColor:'#DAD',
-      marginTop: 20
+      marginTop: 20,
+      left: 23
     },
     //Group Description Text
     groupDescHeaderText: {
       fontFamily: "Open sans",
       fontWeight: "bold",
       fontSize: 15,
-      color: "#3C3C3C"
+      color: "#3C3C3C",
     },
     groupDescText: {
       fontFamily: "Open sans",
       fontSize: 15,
       color: "#7C7B7B",
       lineHeight: 20,
-      marginBottom: 43
+      marginBottom: 43,
+      width:330,
     },
 
     //Group Information Text
@@ -140,19 +108,19 @@ function GroupInfo() {
     },
     //Style location text, since mostly two lines
     giLocationText: {
-        fontFamily: "Open sans",
-        fontSize: 15,
-        color: "#7C7B7B",
-        lineHeight: 20,
-        marginBottom: 16
+      fontFamily: "Open sans",
+      fontSize: 15,
+      color: "#7C7B7B",
+      lineHeight: 20,
+      marginBottom: 16
     },
     //Style for player amount text
     giPlayersText: {
-        fontFamily: "Open sans",
-        fontSize: 29,
-        color:'#094E76',
-        marginBottom: 32,
-        marginTop:-16
+      fontFamily: "Open sans",
+      fontSize: 29,
+      color: "#094E76",
+      marginBottom: 32,
+      marginTop: -16
     }
   });
 
@@ -175,31 +143,27 @@ function GroupInfo() {
         <Text style={styles.giOrganizerText}>Toby Wong</Text>
       </View>
 
-
-      {/* Pink Subheader */}
-      <View style={styles.giSubHeader}>
-        <Image
-          style={styles.groupcloseIcon}
-          source={require("../assets/icon_group_close.png")}
-        />
-        <Text style={styles.giclosingtimeText}>
-          Group closes in<Text style={styles.gispectimeText}> 100 </Text>hours
-        </Text>
-        <Text style={styles.gicostText}>$7</Text>
-      </View>
+      <Bar_group_countdown_price titlePrice={7} countdown={100} />
 
       <View style={styles.giTextSec}>
-        <ScrollView>
-          {/* Group Description */}
-          <Text style={styles.groupDescHeaderText}>Group Description</Text>
-          <Text style={styles.groupDescText}>
-            This is a group description for players who are looking at other
-            created groups cards. So you can only view no editing.
-          </Text>
+        <View
+          style={{
+            flex: 1,
+            position: "absolute",
+            height: 390,
+          }}
+        >
+          <ScrollView style={{ flex: 1 }}>
+            {/* Group Description */}
+            <Text style={styles.groupDescHeaderText}>Group Description</Text>
+            <Text style={styles.groupDescText}>
+              This is a group description for players who are looking at other
+              created groups cards. So you can only view no editing.
+            </Text>
 
-          {/* All group information text */}
-          <View style={{display:'flex',flexDirection:'row'}}>
-            <View style={{flex:1}}>
+            {/* All group information text */}
+            <View style={{ display: "flex", flexDirection: "row" }}>
+              <View style={{ flex: 1 }}>
                 <Text style={styles.giTitleText}>Group</Text>
                 <Text style={styles.giTitleText}>Date</Text>
                 <Text style={styles.giTitleText}>Centre</Text>
@@ -208,28 +172,28 @@ function GroupInfo() {
                 <Text style={styles.giTitleText}>Group Limit</Text>
                 <Text style={styles.giTitleText}>Players</Text>
                 <Text style={styles.giTitleText}>Bird Type</Text>
-            </View>
-            <View style={{flex:1}}>
+              </View>
+              <View style={{ flex: 1 }}>
                 <Text style={styles.giText}>#C1314</Text>
                 <Text style={styles.giText}>30 December 2019</Text>
                 <Text style={styles.giText}>ClearOne</Text>
                 <Text style={styles.giLocationText}>
-                    4351 No 3 Rd #100,{"\n"}Richmond, BC V6X 3A7
+                  4351 No 3 Rd #100,{"\n"}Richmond, BC V6X 3A7
                 </Text>
                 <Text style={styles.giText}>1pm - 4pm</Text>
                 <Text style={styles.giText}>5</Text>
                 <Text style={styles.giPlayersText}>4</Text>
                 <Text style={styles.giText}>Feather</Text>
+              </View>
             </View>
-          </View>
-          {/* Organizer Cards */}
-          <Card_organizer />
-          <Card_organizer />
-        </ScrollView>
+            {/* Organizer Cards */}
+            <Card_members organizer={"Organizer"} />
+            <Card_members />
+          </ScrollView>
+        </View>
       </View>
       {/* Join Button */}
-      <Button_Join />
-
+      <Button_Join price={7} />
     </View>
   );
 }
