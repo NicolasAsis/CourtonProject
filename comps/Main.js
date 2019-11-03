ea4b6aa2f8d3b5152caf097fa688ca4018b24e12
NicolasAsis <nico.asis15@gmail.com>
2019-11-03 15:29:03 -0800

    Started 'o group info' page, added icon to assets

--- comps/Main.js
+++ comps/Main.js
@@ -12,13 +12,14 @@ import Footer_court_indicator from './Footer_court_indicator';
 import Sticky_footer_home from './Sticky_footer_home';
 import LoadingAnimation from './LoadingAnimation';
 import GroupSummary from '../pages/Page-group-summary';
+import Organizer_groupInfo from '../pages/Page-organizer-groupInfo';
 
 
 
 function Main(){
     return(
         <View>
-            <GroupSummary />
+            <Organizer_groupInfo />
         </View>
     );
 }
