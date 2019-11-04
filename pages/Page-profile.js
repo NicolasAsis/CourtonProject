adfe3709c2fd290c701f984c62620778b9d8134a
heiditang <heiditang128@gmail.com>
2019-11-03 16:07:25 -0800

    Can someone help me with the layout?

--- pages/Page-profile.js
+++ pages/Page-profile.js
@@ -1,30 +1,76 @@
 import React, {useState} from 'react';
-import {Text,View, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import {Text,View, Image, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
 
 function Profile(){
     return(
-        <View styes={{fontFamily:'Open Sans'}}>
-            <View style={styles.header}>
-                <Text style={styles.name}>Jacky Lee</Text>
-                <View style={{position: 'absolute'}}>
-                <TouchableOpacity>
-                    <Image style={styles.iconEdit}
-                       source={require("../assets/icon_edit.png")}/>
-                </TouchableOpacity>
+        <View>
+            {/* this is the header part */}
+            <View styes={{fontFamily:'Open Sans'}}>
+                <View style={styles.header}>
+                    <Text style={styles.name}>Jacky Lee</Text>
+                    <View style={{position: 'absolute'}}>
+                    <TouchableOpacity>
+                        <Image style={styles.iconEdit}
+                        source={require("../assets/icon_edit.png")}/>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.profileImg}></View>
-                <TouchableOpacity>
-                    <Image style={styles.iconCamera}
-                       source={require("../assets/icon_camera.png")}/>
-                </TouchableOpacity>
+                
+                    <TouchableOpacity>
+                    <View style={styles.profileImg}>
+                        <Image style={styles.iconCamera}
+                        source={require("../assets/icon_camera.png")}/>
+                    </View>
+                    </TouchableOpacity>
+            </View>
+        </View>
+            {/* this is the group numbers and text */}
+            <View style={styles.groupsText}>
+                <View style={{flex:1}}>
+                    <TouchableOpacity>
+                    <Text style={styles.numberGroups}>2</Text>
+                    <Text style={styles.createdGroups}>Created{"\n"}Groups</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{flex:1}}>
+                    <TouchableOpacity>
+                    <Text style={styles.numberGroups2}>5</Text>
+                    <Text style={styles.joinedGroups}>Joined{"\n"}Groups</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
 
-
-            
+            {/* the other links on the profile page */}
+            <View>
+                <View style={styles.profileIcons}>
+                <TouchableOpacity>
+                    <Image style={{width:25, height: 35}} source={require ('../assets/icon_notification.png')}/>
+                    <Text style={styles.optionsText}>Notfications</Text>
+                 {/* </TouchableOpacity>
+                </View> */}
+                {/* <View style={styles.profileIcons}>
+                <TouchableOpacity> */}
+                    <Image style={{width:25, height: 25, marginBottom: 30}} source={require ('../assets/icon_help.png')}/>
+                    <Text style={styles.optionsText}>Help</Text>               
+                {/* </TouchableOpacity >
+                </View>
+                <View style={styles.profileIcons}>
+                <TouchableOpacity > */}
+                    <Image style={{width:25, height: 25, marginBottom: 30}} source={require ('../assets/icon_faq.png')}/>
+                    <Text style={styles.optionsText}>FAQ</Text>
+                {/* </TouchableOpacity>
+                </View>
+                <View style={styles.profileIcons}>
+                <TouchableOpacity>  */}
+                    <Image style={{width:25, height: 25, marginBottom: 30}} source={require ('../assets/icon_comment.png')}/>
+                    <Text style={styles.optionsText}>About CourtOn</Text>
+                </TouchableOpacity>
+                </View>
+            </View>
         </View>
+
     );
 
-}
+};
 
 const styles = StyleSheet.create({
     header: {
@@ -62,12 +108,79 @@ const styles = StyleSheet.create({
         top: 20,
     },
     iconCamera:{
-        width: 25,
-        height: 25,
-        left: 25,
-        top: -5.8,
+        width: 28,
+        height: 28,
+        left: 73,
+        top: 65,
         position: 'absolute',
     },
+
+    groupsText:{
+        position: 'absolute',
+        width: '100%',
+        top: 266,
+        display:'flex',
+        flexDirection: 'row'
+    },
+
+    numberGroups:{
+        fontWeight: 'bold',
+        fontSize: 25,
+        lineHeight: 34,
+        color: '#4A4A4A',
+        flexDirection: 'row',
+        left: 132,
+    },
+    numberGroups2:{
+        fontWeight: 'bold',
+        fontSize: 25,
+        lineHeight: 34,
+        color: '#4A4A4A',
+        left: 60,
+    
+    },
+    createdGroups:{
+        fontWeight: 'normal',
+        fontSize: 15,
+        lineHeight: 25,
+        color: '#4A4A4A',
+        flexDirection: 'row',
+        textAlign: 'center',
+        left: 40,
+        letterSpacing: 2.5,
+        // top: 40,
+    },
+
+    joinedGroups:{
+        fontWeight: 'normal',
+        fontSize: 15,
+        lineHeight: 25,
+        color: '#4A4A4A',
+        flexDirection: 'row',
+        textAlign: 'center',
+        left: -32,
+        letterSpacing: 2.5,
+
+    },
+    profileIcons:{
+        width: '100%',
+        flex: 1,
+        flexDirection:'column',
+        top: 380,
+        left:140,
+
+    },
+
+    optionsText:{
+        fontWeight: 'normal',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+        color: '#3C3C3C',
+        left: -60,
+        top: -30,
+    }
+    
 
 });
 
