170d8cc3c6d7aeccebe2cf06e875ae7fae8fa2ac
siyayang0420 <siyayang0420@live.com>
2019-11-13 16:49:48 -0800

    finished my-group in-page transition

--- pages/Page-my-group.js
+++ pages/Page-my-group.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -8,11 +8,16 @@ import {
   Animated
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import Card_for_player from "../comps/Card_for_player";
 import Sticky_footer_regular from "../comps/Sticky_footer_regular";
-import Switch_groupType from '../comps/Switch_groupType';
-import SwitchSelector from 'react-native-switch-selector';
+import Switch_groupType from "../comps/Switch_groupType";
+import Card_for_member from "../comps/Card_for_member";
+import Card_for_organizer from "../comps/Card_for_organizer";
+
 function MyGroup() {
+  const [group, setGroup] = useState([]);
+
+  const [groupType, setGroupType] = useState();
+
   // const [linePostionXRight, setPositionXRight] = useState(
   //   new Animated.Value(setPositionXRight ? 200 : 0)
   // );
@@ -27,6 +32,148 @@ function MyGroup() {
   //   outputRange: [0, 200]
   // });
 
+  const LoadJoinedGroup = async () => {
+    var obj = {
+      key: "groups_read",
+      data: {
+        id: id,
+        bmtCentre: bmtCentre,
+        date: date
+      }
+    };
+    // var r = await axios.post("http://142.232.162.71:3001/post", obj);
+    // console.log("read", r.data);
+    // var dbusers = JSON.parse(r.data.body);
+    // console.log("read", dbusers);
+    // setUsers(dbusers.data);
+  };
+
+  const LoadCreatedGroup = async () => {
+    var obj = {
+      key: "groups_read",
+      data: {
+        id: id,
+        organizerName: organizerName,
+        date: date
+      }
+    };
+    // var r = await axios.post("http://142.232.162.71:3001/post", obj);
+    // console.log("read", r.data);
+    // var dbusers = JSON.parse(r.data.body);
+    // console.log("read", dbusers);
+    // setUsers(dbusers.data);
+  };
+
+  const joinedData = [
+    {
+      organizerName: "Tony Wong",
+      groupNum: "1511",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: 0.2
+    },
+    {
+      organizerName: "Dan Dhanika",
+      groupNum: "1141",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: 0.67
+    },
+    {
+      organizerName: "Nico Asis",
+      groupNum: "1311",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: 0.4
+    },
+    {
+      organizerName: "Heidi Tang",
+      groupNum: "2111",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: 0.8
+    },
+    {
+      organizerName: "Siya Yang",
+      groupNum: "0111",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: 0.5
+    }
+  ];
+
+  const createdData = [
+    {
+      bmtCentre: "Stage 18",
+      groupNum: "1511",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: "0.2"
+    },
+    {
+      bmtCentre: "Stage 18",
+      groupNum: "1141",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: "0.67"
+    },
+    {
+      bmtCentre: "Clear One",
+      groupNum: "1311",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: "0.4"
+    },
+    {
+      bmtCentre: "Clear One",
+      groupNum: "2111",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: "0.8"
+    },
+    {
+      bmtCentre: "Clear One",
+      groupNum: "0111",
+      date: "Sat Dec 10",
+      time: "1pm -2pm",
+      joinedMember: 2,
+      totalMember: 10,
+      price: 7,
+      progressBarLoad: "0.5"
+    }
+  ];
+
+  useEffect(() => {
+    LoadJoinedGroup();
+    LoadCreatedGroup();
+  }, []);
 
   return (
     <View>
@@ -37,54 +184,75 @@ function MyGroup() {
         />
         <Text style={styles.txtTitle}>My Groups</Text>
       </View>
-      {/* <SwitchSelector
-                // style={styles.gsSwitch}
-                initial={0}
-                // onPress={value => this.setState({ gender: value })}
-                textColor='#094E76' //'#7a44cf'
-                selectedColor='white'
-                buttonColor='#C73C50'
-                // borderColor='#5DB9F0'
-                borderRadius='false'
-                backgroundColor='#FE647B'
-                textStyle={styles.txtBar}
-                selectedTextStyle={styles.selectedTxtBar}
-                // hasPadding
-                options={[
-                  { 
-                    label: "Joined", 
-                    value: "Joined"
-                  }, 
-                  {
-                    label: "Created",
-                    value: "Created",
-                  } 
-                ]}
-              /> */}
-              <Switch_groupType
-              initial={0}
-              // onPress={value => this.setState({ gender: value })}
-              textColor='#094E76' //'#7a44cf'
-              selectedColor='white'
-              buttonColor='#FE647B'
-              // borderColor='#5DB9F0'
-              borderRadius={0}
-              backgroundColor='#FE647B'
-              textStyle={styles.txtBar}
-              selectedTextStyle={styles.selectedTxtBar}
-              // hasPadding
-              options={[
-                { 
-                  label: "Joined", 
-                  value: "Joined"
-                }, 
-                {
-                  label: "Created",
-                  value: "Created",
-                } 
-              ]}
-              />
-              
+      <Switch_groupType
+        initial={0}
+        // onPress={value => this.setState({ gender: value })}
+        textColor="#094E76"
+        selectedColor="white"
+        buttonColor="#FE647B"
+        // borderColor='#5DB9F0'
+        borderRadius={0}
+        backgroundColor="#FE647B"
+        textStyle={styles.txtBar}
+        selectedTextStyle={styles.selectedTxtBar}
+        // hasPadding
+        options={[
+          {
+            label: "Joined",
+            value: "Joined"
+          },
+          {
+            label: "Created",
+            value: "Created"
+          }
+        ]}
+        onPress={(val) => {
+          if (val == "Joined") {
+            setGroupType(
+              <View>
+                {joinedData.map(obj => {
+                  return (
+                    <Card_for_member
+                      // key = {i}
+                      id={obj.id}
+                      organizerName={obj.organizerName}
+                      groupNum={obj.groupNum}
+                      date={obj.date}
+                      time={obj.time}
+                      joinedMember={obj.joinedMember}
+                      totalMember={obj.totalMember}
+                      price={obj.price}
+                      progressBarLoad={obj.progressBarLoad}
+                    />
+                  );
+                })}
+              </View>
+            );
+          } else if (val == "Created") {
+            setGroupType(
+              <View>
+                {createdData.map(obj => {
+                  return (
+                    <Card_for_organizer
+                      // key = {i}
+                      id={obj.id}
+                      bmtCentre={obj.bmtCentre}
+                      groupNum={obj.groupNum}
+                      date={obj.date}
+                      time={obj.time}
+                      joinedMember={obj.joinedMember}
+                      totalMember={obj.totalMember}
+                      price={obj.price}
+                      progressBarLoad={obj.progressBarLoad}
+                    />
+                  );
+                })}
+              </View>
+            );
+          }
+        }}
+      />
+
       {/* <View style={styles.subTitleBar}>
         <TouchableOpacity
           style={styles.btnBar}
@@ -117,16 +285,44 @@ function MyGroup() {
       >
         <View style={styles.selectedLine}></View>
       </Animated.View> */}
-      <View style={{backgroundColor:'#FFFFFF' }}>
+      <View style={{ backgroundColor: "#FFFFFF" }}>
         <ScrollView style={{ height: 612 }}>
-          <View style={{ marginBottom: 100}}>
-            <Card_for_player />
-            <Card_for_player />
-            <Card_for_player />
-            <Card_for_player />
-            <Card_for_player />
-            <Card_for_player />
-            <Card_for_player />
+          <View style={{ marginBottom: 100 }}>
+            {/* {joinedData.map((obj,i)=>{
+                    return <Card_for_member
+                    // key = {i}
+                    id = {obj.id}
+                    organizerName = {obj.organizerName}
+                    groupNum = {obj.groupNum}
+                    date = {obj.date}
+                    time = {obj.time}
+                    joinedMember = {obj.joinedMember}
+                    totalMember = {obj.totalMember}
+                    price = {obj.price}
+                    progressBarLoad = {obj.progressBarLoad}
+
+                />
+                })
+            }
+
+            {createdData.map((obj,i)=>{
+                    return <Card_for_organizer
+                    // key = {i}
+                    id = {obj.id}
+                    bmtCentre = {obj.bmtCentre}
+                    groupNum = {obj.groupNum}
+                    date = {obj.date}
+                    time = {obj.time}
+                    joinedMember = {obj.joinedMember}
+                    totalMember = {obj.totalMember}
+                    price = {obj.price}
+                    progressBarLoad = {obj.progressBarLoad}
+
+                />
+                })
+            } */}
+
+            {groupType}
           </View>
         </ScrollView>
       </View>
@@ -164,7 +360,7 @@ const styles = StyleSheet.create({
     fontFamily: "Open sans",
     color: "#FFFFFF",
     textAlign: "center",
-    bottom:2
+    bottom: 2
   },
   txtBar: {
     flex: 1,
@@ -172,7 +368,7 @@ const styles = StyleSheet.create({
     fontFamily: "Open sans",
     color: "#FFFFFF",
     textAlign: "center",
-    bottom:2
+    bottom: 2
   },
   btnBar: {
     width: "50%",
